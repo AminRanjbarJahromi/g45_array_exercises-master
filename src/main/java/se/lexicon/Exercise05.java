fbeca5f030271bed605e27660c4da675dbcc3bdb
Amin Ranjbar <m.amin.ranjbar@gmail.com>
2022-10-21 10:29:30 +0200

    improve answers

--- src/main/java/se/lexicon/Exercise05.java
+++ src/main/java/se/lexicon/Exercise05.java
@@ -9,6 +9,19 @@ public class Exercise05 {
    *              Sweden Stockholm
    */
   public static void ex5() {
+    String[][] region = new String[2][2];
+    region[0][0] = "France";
+    region[0][1] = "Paris";
+
+    region[1][0] = "Sweden";
+    region[1][1] = "Stockholm";
+    for (int i = 0; i < region.length; i++) {
+      for (int j = 0; j < region[i].length; j++) {
+        System.out.print(region[i][j] + "\t");
+      }
+      System.out.println("\n");
+
+    }
 
   }
 
--- src/main/java/se/lexicon/Exercise06.java
+++ src/main/java/se/lexicon/Exercise06.java
@@ -1,5 +1,8 @@
 package se.lexicon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Exercise06 {
 
   /**
@@ -7,6 +10,20 @@ public class Exercise06 {
    * Expected output: Average is: 17.3
    */
   public static void ex6() {
+    double [] numbers = {43, 5, 23, 17, 2};
+    double total = 0;
+    double [] newArrayNumbers = Arrays.copyOf(numbers,numbers.length+1);
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Import new number : ");
+    double input = sc.nextInt();
+    newArrayNumbers[newArrayNumbers.length-1] = input;
+    for(int i = 0 ; i< numbers.length ; i++){
+      total = total + numbers[i];
+    }
+    double average = total / numbers.length;
+    System.out.println(" Average is : " + average);
+
+
 
   }
 
